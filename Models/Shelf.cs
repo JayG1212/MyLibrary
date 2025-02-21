567f64e91b60f9df215cf54ccebb24f408d6d29c
JayG1212 <jaybgunderson@gmail.com>
2025-02-21 14:03:39 -0600

    Update Shelf.cs

--- Models/Shelf.cs
+++ Models/Shelf.cs
@@ -2,10 +2,21 @@
 {
     public class Shelf
     {
-        private int ShelfID = -1;
+        private int shelfID = -1;
         private int bookID = -1;
 
         // Accessors and Mutators
+        public int ShelfID
+        {
+            get { return this.shelfID; }
+            set { this.shelfID = value; }
+        }
+
+        public int BookID
+        {
+            get { return this.bookID; }
+            set {this.bookID = value; }
+        }
 
     }
 }
