3eecb7279860e4979ee373626fde30d2c99caa91
JayG1212 <133731996+JayG1212@users.noreply.github.com>
2025-02-23 01:06:12 -0600

    Other Controllers made

---
+++ Controllers/ShelvesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyLibrary.Data;
+using MyLibrary.Models;
+
+namespace MyLibrary.Controllers
+{
+    public class ShelvesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShelvesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shelves
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Shelves.ToListAsync());
+        }
+
+        // GET: Shelves/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shelf = await _context.Shelves
+                .FirstOrDefaultAsync(m => m.ShelfID == id);
+            if (shelf == null)
+            {
+                return NotFound();
+            }
+
+            return View(shelf);
+        }
+
+        // GET: Shelves/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shelves/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ShelfID,ShelfName")] Shelf shelf)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shelf);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shelf);
+        }
+
+        // GET: Shelves/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shelf = await _context.Shelves.FindAsync(id);
+            if (shelf == null)
+            {
+                return NotFound();
+            }
+            return View(shelf);
+        }
+
+        // POST: Shelves/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ShelfID,ShelfName")] Shelf shelf)
+        {
+            if (id != shelf.ShelfID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shelf);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShelfExists(shelf.ShelfID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shelf);
+        }
+
+        // GET: Shelves/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shelf = await _context.Shelves
+                .FirstOrDefaultAsync(m => m.ShelfID == id);
+            if (shelf == null)
+            {
+                return NotFound();
+            }
+
+            return View(shelf);
+        }
+
+        // POST: Shelves/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shelf = await _context.Shelves.FindAsync(id);
+            if (shelf != null)
+            {
+                _context.Shelves.Remove(shelf);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShelfExists(int id)
+        {
+            return _context.Shelves.Any(e => e.ShelfID == id);
+        }
+    }
+}
---
+++ Migrations/20250223065253_InitialCreate.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.AuthorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    GenreID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.GenreID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shelf",
+                columns: table => new
+                {
+                    ShelfID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShelfName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shelf", x => x.ShelfID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorID = table.Column<int>(type: "int", nullable: false),
+                    GenreID = table.Column<int>(type: "int", nullable: false),
+                    ShelfID = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicationYear = table.Column<int>(type: "int", nullable: false),
+                    Pages = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookID);
+                    table.ForeignKey(
+                        name: "FK_Book_Author_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Author",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Book_Genre_GenreID",
+                        column: x => x.GenreID,
+                        principalTable: "Genre",
+                        principalColumn: "GenreID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Book_Shelf_ShelfID",
+                        column: x => x.ShelfID,
+                        principalTable: "Shelf",
+                        principalColumn: "ShelfID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorID",
+                table: "Book",
+                column: "AuthorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_GenreID",
+                table: "Book",
+                column: "GenreID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_ShelfID",
+                table: "Book",
+                column: "ShelfID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropTable(
+                name: "Shelf");
+        }
+    }
+}
